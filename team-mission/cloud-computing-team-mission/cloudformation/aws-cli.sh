f66347aeabb952f6ae1f1b870f5d0450295f8cce
JaeHwan Kim <iwantbaobab@gmail.com>
2025-05-12 16:48:56 +0900

    add: Security Group 코드 추가

--- team-mission/cloud-computing-team-mission/cloudformation/aws-cli.sh
+++ team-mission/cloud-computing-team-mission/cloudformation/aws-cli.sh
@@ -3,6 +3,7 @@ filePath="file:///Users/jaehwan/Desktop/goormthon/team-mission/cloud-computing-t
 # 템플릿 유효성 검사
 aws cloudformation validate-template --template-body $filePath/networking/VPC.yaml
 aws cloudformation validate-template --template-body $filePath/networking/VPC-subnets.yaml
+aws cloudformation validate-template --template-body $filePath/networking/sg-web.yaml
 aws cloudformation validate-template --template-body $filePath/compute/EC2.yaml
 
 # 템플릿을 활용해 stack 생성
@@ -14,6 +15,12 @@ aws cloudformation create-stack \
 aws cloudformation create-stack \
   --stack-name cfn-goorm-vpc-subnets \
   --template-body $filePath/networking/vpc-subnets.yaml \
+  --parameters $filePath/parameters/parameter.yaml \
+  --capabilities CAPABILITY_IAM
+
+aws cloudformation create-stack \
+  --stack-name cfn-goorm-sg-web \
+  --template-body $filePath/networking/sg-web.yaml \
   --parameters $filePath/parameters/parameter.yaml \
   --capabilities CAPABILITY_IAM
 
