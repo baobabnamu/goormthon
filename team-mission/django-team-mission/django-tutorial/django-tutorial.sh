0f4f892f6d467bd1d1da13941c7f73ac1ed46dcd
JaeHwan Kim <iwantbaobab@gmail.com>
2025-05-16 17:26:22 +0900

    fix: django-tutorial.sh 명령어 오류 수정

--- team-mission/django-team-mission/django-tutorial/django-tutorial.sh
+++ team-mission/django-team-mission/django-tutorial/django-tutorial.sh
@@ -2,7 +2,7 @@
 python manage.py startapp polls
 
 # 프로젝트 디렉토리에서 가상환경 활성화한 상태로 진행
-source ../django-tutorial-env/venv/bin/activate
+source ../django-tutorial-env/bin/activate
 
 # mysql 연동 관련 라이브러리 설치
 pip install pkg-config
